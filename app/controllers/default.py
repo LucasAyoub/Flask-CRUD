f2006344a384995d9f7921bc5abc886c1946c6be
Lucas <lucas.barbosa10622@gmail.com>
2021-01-21 13:11:38 -0200

    First Commit

---
+++ app/controllers/default.py
@@ -0,0 +1,11 @@
+from app import app
+from flask import Flask, render_template, session
+
+
+@app.route("/")
+def Index():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
